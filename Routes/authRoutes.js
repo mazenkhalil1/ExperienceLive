8180b17d796dbc7d9e3508290c918af22ea3cd74
Mazensead12345 <162707971+Mazensead12345@users.noreply.github.com>
2025-04-14 21:15:10 +0200

    yarabb

--- Routes/authRoutes.js
+++ Routes/authRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 //const authController = require('../controllers/userController');
 
 
-const authController = require('../controllers/authController');
+const {register} = require('../controllers/authController');
 router.post('/register', authController.register);
  // Ensure this route is correctly defined
 //router.post('/login', login);
