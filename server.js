1991c900d85fd378caf9f7ed20a9d52bfdaa4d22
Mazensead12345 <162707971+Mazensead12345@users.noreply.github.com>
2025-04-14 18:26:33 +0200

    changed in server

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ dotenv.config();
 
 const app = express();
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+  //useNewUrlParser: true,
+ // useUnifiedTopology: true,
 })
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.error('MongoDB connection failed:', err));
