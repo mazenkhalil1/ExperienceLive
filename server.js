56d4ec8c1ec2b97478e5745dd12dc508ce35663f
Mazensead12345 <162707971+Mazensead12345@users.noreply.github.com>
2025-04-14 18:13:46 +0200

    ss

--- server.js
+++ server.js
@@ -8,7 +8,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require('dotenv');
-const cookieParser=require('cookie-parser')
+//const cookieParser=require('cookie-parser')
 const cors = require("cors");
 
 dotenv.config();
@@ -37,7 +37,7 @@ const authenticationMiddleware=require('./middleware/authenticationmiddleware')
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(cookieParser())
+//app.use(cookieParser())
 
 app.use(
   cors({
