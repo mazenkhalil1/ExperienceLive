468d91b8e426cc027aa3e346c8d9fb8c0aa00790
Mazensead12345 <162707971+Mazensead12345@users.noreply.github.com>
2025-04-14 20:23:46 +0200

    j

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 //const courseRouter = require("./Routes/course");
-const userRouter = require("./Routes/userRoutes");
+const userRouter = require("./Routes/usersRoutes");
 const authRoutes = require("./Routes/authRoutes");
 //const authenticationMiddleware=require('./middleware/authenticationmiddleware')
 
