130baa6d9c6d91dd305629885330a38d361c787a
Mazensead12345 <162707971+Mazensead12345@users.noreply.github.com>
2025-04-14 18:16:11 +0200

    ss

--- server.js
+++ server.js
@@ -28,10 +28,10 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-const courseRouter = require("./Routes/course");
-const userRouter = require("./Routes/user");
-const authRouter = require("./Routes/auth");
-const authenticationMiddleware=require('./middleware/authenticationmiddleware')
+//const courseRouter = require("./Routes/course");
+//const userRouter = require("./Routes/user");
+//const authRouter = require("./Routes/auth");
+//const authenticationMiddleware=require('./middleware/authenticationmiddleware')
 
 
 app.use(express.json());
