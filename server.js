edd885d046537514c5e6052527c053ce6f977ef5
Mazensead12345 <162707971+Mazensead12345@users.noreply.github.com>
2025-04-14 18:10:34 +0200

    s

--- server.js
+++ server.js
@@ -2,6 +2,18 @@
 
 
 
+
+
+
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require('dotenv');
+const cookieParser=require('cookie-parser')
+const cors = require("cors");
+
+dotenv.config();
+
+const app = express();
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -15,17 +27,6 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-
-const express = require("express");
-const mongoose = require("mongoose");
-const dotenv = require('dotenv');
-const cookieParser=require('cookie-parser')
-const cors = require("cors");
-
-dotenv.config();
-
-const app = express();
 
 const courseRouter = require("./Routes/course");
 const userRouter = require("./Routes/user");
