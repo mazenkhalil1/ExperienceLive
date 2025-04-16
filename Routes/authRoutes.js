55bb3c1a7b556e3b24c223ce919c5aba782e5703
Mazensead12345 <162707971+Mazensead12345@users.noreply.github.com>
2025-04-16 11:57:51 +0200

    fixed

--- Routes/authRoutes.js
+++ Routes/authRoutes.js
@@ -1,15 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const authController = require('../controllers/authController');
 
-const authController= require('../controllers/authController');
-//const authorizationMiddleware = require('../middleware/authorizationmiddleware');
-
-router.post('/register',authController.register);
-
+router.post('/register', authController.register);
 router.post('/login', authController.login);
 
- // Ensure this route is correctly defined
-//router.post('/login', login);
-
 module.exports = router;
-
