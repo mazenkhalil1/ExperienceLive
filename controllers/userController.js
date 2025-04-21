c03e6db83192b4363b439c5367da178842b2de82
malakmeabed <malak.meabed@giu.giu-uni.de>
2025-04-21 18:19:15 +0200

    change

--- controllers/userController.js
+++ controllers/userController.js
@@ -40,7 +40,7 @@ exports.getUser = async (req, res, next) => {
 // Get current user's profile
 exports.getProfile = async (req, res, next) => {
   try {
-    const user = await User.findById(req.user.userId).select("-password");
+    const user = await User.findById(req.user.userId).select("+password");
 
     if (!user) {
       return res.status(404).json({
@@ -59,19 +59,24 @@ exports.getProfile = async (req, res, next) => {
 };
 
 // Update current user's profile
+const bcrypt = require("bcrypt");
 exports.updateProfile = async (req, res, next) => {
   try {
     const updates = {
       name: req.body.name,
       email: req.body.email,
-      password: req.body.password,
       profilePicture: req.body.profilePicture
     };
+
+    if(req.body.password) {
+      const salt = await bcrypt.genSalt(10);
+      updates.password = await bcrypt.hash(req.body.password, salt);
+    }
 
     const user = await User.findByIdAndUpdate(req.user.userId, updates, {
       new: true,
       runValidators: true
-    }).select("-password");
+    }).select("+password");
 
     if (!user) {
       return res.status(404).json({
