5934ddf05962a0378c9fa0bc242d59210b1d5cc3
malakmeabed <malak.meabed@giu.giu-uni.de>
2025-04-21 15:53:57 +0200

    user

--- Routes/usersRoutes.js
+++ Routes/usersRoutes.js
@@ -5,16 +5,24 @@ const authMiddleware = require("../middleware/authenticationmiddleware");
 const authorize = require("../middleware/authorizationmiddleware");
 
 // Admin routes
-router.get("/", authMiddleware, authorize(["admin"]), userController.getUsers);
+//router.get("/", authMiddleware, authorize(["admin"]), userController.getUsers);
 router.get("/profile", authMiddleware, userController.getProfile);
 router.put("/profile", authMiddleware, userController.updateProfile);
-router.get("/:id", authMiddleware, authorize(["admin"]), userController.getUser);
-router.put("/:id", authMiddleware, authorize(["admin"]), userController.updateUserRole);
-router.delete("/:id", authMiddleware, authorize(["admin"]), userController.deleteUser);
 router.get("/bookings", authMiddleware, userController.getUserBookings);
 
 // Organizer event routes
 router.get("/events", authMiddleware, authorize(["organizer"]), userController.getOrganizerEvents);
 router.get("/events/analytics", authMiddleware, authorize(["organizer"]), userController.getEventAnalytics);
+
+router.get("/", authMiddleware, authorize(["admin"]), userController.getUsers);
+
+router.get("/:id", authMiddleware, authorize(["admin"]), userController.getUser);
+router.put("/:id", authMiddleware, authorize(["admin"]), userController.updateUserRole);
+router.delete("/:id", authMiddleware, authorize(["admin"]), userController.deleteUser);
+//router.get("/bookings", authMiddleware, userController.getUserBookings);
+
+// Organizer event routes
+//router.get("/events", authMiddleware, authorize(["organizer"]), userController.getOrganizerEvents);
+//router.get("/events/analytics", authMiddleware, authorize(["organizer"]), userController.getEventAnalytics);
 
 module.exports = router;
